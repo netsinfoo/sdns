226576b654bf8aa69589a42fb288e2df3a5d5853
Netsinfoo <netsinfoo@gmail.com>
2020-10-16 21:28:56 +0000

    Passo 8

--- sdns/navegation.py
+++ sdns/navegation.py
@@ -1,30 +1,20 @@
 from extras.plugins import PluginMenuButton, PluginMenuItem
 from utilities.choices import ButtonColorChoices
 
-
 menu_items = (
     PluginMenuItem(
         link='plugins:sdns:register_list',
         link_text='Registers',
-        permissions=['sdns.view_register'],
+        permissions=[],
         buttons=(
             PluginMenuButton(
                 link='plugins:sdns:register_add',
                 title='Add a new register',
                 icon_class='fa fa-plus',
-                color=ButtonColorChoices.GREEN,
-                permissions=['sdns.add_register']
             ),
-        )
-    ),
-    PluginMenuItem(
-        link='plugins:sdns:register_list',
-        link_text='Add a new register 2',
-        permissions=['sdns.view_register'],
-        buttons=(
             PluginMenuButton(
-                link='plugins:sdns:register_add',
-                title='Assign a register 2',
+                link='admin:sdns',
+                title='Add a new animal',
                 icon_class='fa fa-plus',
                 color=ButtonColorChoices.GREEN,
                 permissions=['sdns.add_register']
